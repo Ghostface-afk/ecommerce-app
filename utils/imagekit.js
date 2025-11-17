e700ae8009c764e894bae06160b0c4a931976bdb
Ghostface-afk <lawdanga620@gmail.com>
2025-11-17 16:04:17 +0300

    updated backend 5

--- utils/imagekit.js
+++
@@ -1,9 +0,0 @@
-const ImageKit = require("imagekit");
-
-const imagekit = new ImageKit({
-    publicKey: "YOUR_PUBLIC_API_KEY",
-    privateKey: "YOUR_PRIVATE_API_KEY",
-    urlEndpoint: "https://ik.imagekit.io/YOUR_IMAGEKIT_ID/"
-});
-
-module.exports = imagekit;
